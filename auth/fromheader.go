61dadf5609ff598153e5aa93d3226bfbba1112ea
dcrauwels <d.crauwels@gmail.com>
2025-06-22 21:03:10 +0200

    refactored the two from header functions a bit to deduplicate

--- auth/fromheader.go
+++ auth/fromheader.go
@@ -19,69 +19,50 @@ type databaseQueryer interface {
 	GetVisitorByID(context.Context, uuid.UUID) (database.Visitor, error)
 }
 
-func UserFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (database.User, error) {
-	// use this function when you are performing an action that assumes a logged in user
-	// returns the user in question
-	// return user from header authentication
-	accessingUser := database.User{}
+func AuthFromHeader[T any](
+	w http.ResponseWriter,
+	r *http.Request,
+	cfg configReader,
+	expectedUserType string,
+	queryFunc func(context.Context, uuid.UUID) (T, error),
+) (T, error) {
+	// generic function to generate UserFromHeader and VisitorFromHeader without having to write the same function twice
+	var zero T
 	accessToken, err := GetBearerToken(r.Header)
 	if err != nil {
 		jsonutils.WriteError(w, 401, err, "no authorization field in request header")
-		return accessingUser, err
-	}
-	//validate token
-	accessingUserID, userType, err := ValidateJWT(accessToken, cfg.GetSecret())
-	if err != nil {
-		jsonutils.WriteError(w, 403, err, "access token invalid")
-		return accessingUser, err
-	} else if userType != "user" {
-		jsonutils.WriteError(w, 400, err, "not logged in as user")
-		return accessingUser, err
-	}
-	//query for user by ID and run checks
-	accessingUser, err = db.GetUserByID(r.Context(), accessingUserID)
-	if err == sql.ErrNoRows {
-		jsonutils.WriteError(w, 404, err, "user not found")
-		return accessingUser, err
-	} else if err != nil {
-		jsonutils.WriteError(w, 500, err, "error querying database")
-		return accessingUser, err
+		return zero, err
 	}
 
-	return accessingUser, nil
+	//validate token
+	id, userType, err := ValidateJWT(accessToken, cfg.GetSecret())
+	if err != nil {
+		jsonutils.WriteError(w, 403, err, "access token invalid")
+		return zero, err
+	} else if userType != "user" {
+		jsonutils.WriteError(w, 400, err, "not logged in as "+expectedUserType)
+		return zero, err
+	}
+
+	//query for user by ID and run checks
+	entity, err := queryFunc(r.Context(), id)
+	if err == sql.ErrNoRows {
+		jsonutils.WriteError(w, 404, err, expectedUserType+" not found")
+		return zero, err
+	} else if err != nil {
+		jsonutils.WriteError(w, 500, err, "error querying database")
+		return zero, err
+	}
+
+	return entity, nil
+}
+
+func UserFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (database.User, error) {
+	return AuthFromHeader(w, r, cfg, "user", db.GetUserByID)
 }
 
 func VisitorFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (database.Visitor, error) {
-	// same idea as UserFromHeader(). Bit of repetition in first 10 lines or so
-	// this may turn out to be a bad idea.
-	accessingVisitor := database.Visitor{}
-	accessToken, err := GetBearerToken(r.Header)
-	if err != nil {
-		jsonutils.WriteError(w, 403, err, "no authorization field in request header")
-		return accessingVisitor, err
-	}
-
-	//validate token
-	accessingVisitorID, userType, err := ValidateJWT(accessToken, cfg.GetSecret())
-	if err != nil {
-		jsonutils.WriteError(w, 400, err, "access token invalid")
-		return accessingVisitor, err
-	} else if userType != "visitor" {
-		jsonutils.WriteError(w, 403, err, "not logged in as visitor")
-		return accessingVisitor, err
-	}
-
-	//query for visitor by ID and run checks
-	accessingVisitor, err = db.GetVisitorByID(r.Context(), accessingVisitorID)
-	if err == sql.ErrNoRows {
-		jsonutils.WriteError(w, 404, err, "visitor not found")
-		return accessingVisitor, err
-	} else if err != nil {
-		jsonutils.WriteError(w, 500, err, "error querying database (GetVisitorByID)")
-		return accessingVisitor, err
-	}
-
-	return accessingVisitor, nil
+	return AuthFromHeader(w, r, cfg, "visitor", db.GetVisitorByID)
 }
 
 func IsAdminFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (bool, error) {
