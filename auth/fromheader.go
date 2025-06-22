21bf51ee0deebe83843407e70eb7b2dc0504bb2f
dcrauwels <d.crauwels@gmail.com>
2025-06-22 20:16:09 +0200

    added visitorfromheader. wonder if i can deduplicate code here, probably an interface issue.

--- auth/fromheader.go
+++ auth/fromheader.go
@@ -10,29 +10,36 @@ import (
 	"github.com/google/uuid"
 )
 
-type authDependencies interface {
-	GetUserByID(context.Context, uuid.UUID) (database.User, error)
+type configReader interface {
 	GetSecret() string
 }
 
-func UserFromHeader(w http.ResponseWriter, r *http.Request, deps authDependencies) (database.User, error) {
+type databaseQueryer interface {
+	GetUserByID(context.Context, uuid.UUID) (database.User, error)
+	GetVisitorByID(context.Context, uuid.UUID) (database.Visitor, error)
+}
+
+func UserFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (database.User, error) {
 	// use this function when you are performing an action that assumes a logged in user
 	// returns the user in question
 	// return user from header authentication
 	accessingUser := database.User{}
 	accessToken, err := GetBearerToken(r.Header)
 	if err != nil {
-		jsonutils.WriteError(w, 401, err, "no authorization field in header")
+		jsonutils.WriteError(w, 401, err, "no authorization field in request header")
 		return accessingUser, err
 	}
 	//validate token
-	accessingUserID, err := ValidateJWT(accessToken, deps.GetSecret())
+	accessingUserID, userType, err := ValidateJWT(accessToken, cfg.GetSecret())
 	if err != nil {
-		jsonutils.WriteError(w, 401, err, "access token invalid")
+		jsonutils.WriteError(w, 403, err, "access token invalid")
+		return accessingUser, err
+	} else if userType != "user" {
+		jsonutils.WriteError(w, 400, err, "not logged in as user")
 		return accessingUser, err
 	}
 	//query for user by ID and run checks
-	accessingUser, err = deps.GetUserByID(r.Context(), accessingUserID)
+	accessingUser, err = db.GetUserByID(r.Context(), accessingUserID)
 	if err == sql.ErrNoRows {
 		jsonutils.WriteError(w, 404, err, "user not found")
 		return accessingUser, err
@@ -44,9 +51,42 @@ func UserFromHeader(w http.ResponseWriter, r *http.Request, deps authDependencie
 	return accessingUser, nil
 }
 
-func IsAdminFromHeader(w http.ResponseWriter, r *http.Request, deps authDependencies) (bool, error) {
+func VisitorFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (database.Visitor, error) {
+	// same idea as UserFromHeader(). Bit of repetition in first 10 lines or so
+	// this may turn out to be a bad idea.
+	accessingVisitor := database.Visitor{}
+	accessToken, err := GetBearerToken(r.Header)
+	if err != nil {
+		jsonutils.WriteError(w, 403, err, "no authorization field in request header")
+		return accessingVisitor, err
+	}
+
+	//validate token
+	accessingVisitorID, userType, err := ValidateJWT(accessToken, cfg.GetSecret())
+	if err != nil {
+		jsonutils.WriteError(w, 400, err, "access token invalid")
+		return accessingVisitor, err
+	} else if userType != "visitor" {
+		jsonutils.WriteError(w, 403, err, "not logged in as visitor")
+		return accessingVisitor, err
+	}
+
+	//query for visitor by ID and run checks
+	accessingVisitor, err = db.GetVisitorByID(r.Context(), accessingVisitorID)
+	if err == sql.ErrNoRows {
+		jsonutils.WriteError(w, 404, err, "visitor not found")
+		return accessingVisitor, err
+	} else if err != nil {
+		jsonutils.WriteError(w, 500, err, "error querying database (GetVisitorByID)")
+		return accessingVisitor, err
+	}
+
+	return accessingVisitor, nil
+}
+
+func IsAdminFromHeader(w http.ResponseWriter, r *http.Request, cfg configReader, db databaseQueryer) (bool, error) {
 	// return IsAdmin bool from header authentication
-	accessingUser, err := UserFromHeader(w, r, deps)
+	accessingUser, err := UserFromHeader(w, r, cfg, db)
 	if err != nil {
 		return false, err
 	}
